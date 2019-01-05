b780737c3eaa37ea2d0f3421b332f23562a11408
Zoltan Kochan <z@kochan.io>
2019-01-05 14:50:54 +0200

    fix: don't override error codes

--- src/index.ts
+++ src/index.ts
@@ -9,6 +9,7 @@ export default async function readPkg (pkgPath: string): Promise<PackageJson> {
   try {
     return await readPackageJson(pkgPath)
   } catch (err) {
+    if (err['code']) throw err // tslint:disable-line
     const pnpmError = new Error(`${pkgPath}: ${err.message}`)
     pnpmError['code'] = 'ERR_PNPM_BAD_PACKAGE_JSON' // tslint:disable-line
     throw pnpmError
--- test/index.ts
+++ test/index.ts
@@ -24,3 +24,14 @@ test('readPackageJson() throw error when name is invalid', async (t) => {
   t.equal(err.code, 'ERR_PNPM_BAD_PACKAGE_JSON')
   t.end()
 })
+
+test('readPackageJson() throw initial error when package.json not found', async (t) => {
+  let err
+  try {
+    await readPackageJson(path.join(fixtures, 'package.json'))
+  } catch (_) {
+    err = _
+  }
+  t.equal(err.code, 'ENOENT')
+  t.end()
+})
